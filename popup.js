d644d557acd3de7085a5e7346ec87d424a2f95ac
Maria <maria@communitylend.com>
2013-06-24 09:51:27 -0400

    add saving word to localstorage

--- popup.js
+++ popup.js
@@ -1,12 +1,41 @@
-
-function getSelected(o){
-  document.getElementById("word").value = o.word; 
+Word = function(word, tag){
+  this.word = word;
+  this.tag = tag;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+$(function () {
   var bkg = chrome.extension.getBackgroundPage();
   bkg.getPageSelection(getSelected);
-  //bkg.callSelected();
-}); 
 
+  if (!localStorage["words"]) {localStorage["words"] = JSON.stringify([]);}
 
+  $("#tryText").html(localStorage["word"]);
+
+  $("form").submit(function(){
+    if (supports_html5_storage()){
+        word = $("#word").val();
+        tag = $("#tag").val();
+        $("#tryText").html(word);//localStorage["word"]);
+        
+        storedWords = JSON.parse(localStorage["words"]);
+        storedWords.push(new Word(word, tag));
+        localStorage["words"] = JSON.stringify(storedWords);
+      }else{
+        alert("Can't save, update your browser!")
+      }
+
+    return false;
+  })
+})
+
+function getSelected(o){
+  $("#word").val(o.word);
+}
+
+function supports_html5_storage() {
+  try {
+    return 'localStorage' in window && window['localStorage'] !== null;
+  } catch (e) {
+    return false;
+  }
+}

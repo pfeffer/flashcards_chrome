d8006c3fbeaf6c4edac139f5a4f1e984fe94725e
Maria <maria@communitylend.com>
2013-09-02 12:42:45 -0400

    more changes

--- popup.js
+++ popup.js
@@ -1,28 +1,67 @@
+var chrm = {};
+chrm.indexedDB = {};
+chrm.indexedDB.db = null;
+chrm.storage_name = "words";
+
 Word = function(word, tag){
   this.word = word;
   this.tag = tag;
+}
+
+
+chrm.indexedDB.open = function() {
+  var request = indexedDB.open(chrm.storage_name, 1);
+  
+  request.onsuccess = function(e){
+    chrm.indexedDB.db = e.target.result;
+  }
+
+  request.onupgradeneeded = function(e){
+    var db = e.target.result;
+    e.target.transaction.onerror = chrm.indexedDB.onerror;
+
+    var store = db.createObjectStore(chrm.storage_name,
+      {keyPath: "id", autoIncrement: true })
+    store.createIndex("word", "word", { unique: false });
+    store.createIndex("tags", "tag", { unique: false });
+  }
+  request.onerror = chrm.indexedDB.onerror;
+}
+
+chrm.indexedDB.addWord = function(word){
+  var db = chrm.indexedDB.db;
+  var transaction = db.transaction([chrm.storage_name], "readwrite");
+  var store = transaction.objectStore(chrm.storage_name);
+  var request = store.put(word);
+
+  request.onsuccess = function(e){
+    console.log("saved!");
+  }
+  request.onerror = function(e){
+    console.log(e.value);
+  }
 }
 
 $(function () {
   var bkg = chrome.extension.getBackgroundPage();
   bkg.getPageSelection(getSelected);
 
-  if (!localStorage["words"]) {localStorage["words"] = JSON.stringify([]);}
-
   $("#tryText").html(localStorage["word"]);
+  chrm.indexedDB.open();
 
   $("form").submit(function(){
-    if (supports_html5_storage()){
+    // if (supports_html5_storage()){
         word = $("#word").val();
         tag = $("#tag").val();
-        $("#tryText").html(word);//localStorage["word"]);
+        chrm.indexedDB.addWord(new Word(word, tag));
+    //     $("#tryText").html(word);//localStorage["word"]);
         
-        storedWords = JSON.parse(localStorage["words"]);
-        storedWords.push(new Word(word, tag));
-        localStorage["words"] = JSON.stringify(storedWords);
-      }else{
-        alert("Can't save, update your browser!")
-      }
+    //     storedWords = JSON.parse(localStorage["words"]);
+    //     storedWords.push(new Word(word, tag));
+    //     localStorage["words"] = JSON.stringify(storedWords);
+    //   }else{
+    //     alert("Can't save, update your browser!")
+    //   }
 
     return false;
   })
@@ -30,12 +69,4 @@ $(function () {
 
 function getSelected(o){
   $("#word").val(o.word);
-}
-
-function supports_html5_storage() {
-  try {
-    return 'localStorage' in window && window['localStorage'] !== null;
-  } catch (e) {
-    return false;
-  }
 }
